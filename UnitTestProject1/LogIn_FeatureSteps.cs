5bbfaba85cecf9f094098ebf56de52b86fcdbaa7
Zhabolenko Igor <izhabol@gmail.com>
2017-07-20 14:31:42 +0300

    first commit

---
+++ UnitTestProject1/LogIn_FeatureSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace UnitTestProject1
+{
+    [Binding]
+    public class LogIn_FeatureSteps
+    {
+        [Given(@"User is at the Login Window")]
+        public void GivenUserIsAtTheLoginWindow()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"User is logged in")]
+        public void GivenUserIsLoggedIn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"User enter UserName and Password")]
+        public void WhenUserEnterUserNameAndPassword()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Click on the LogIn button")]
+        public void WhenClickOnTheLogInButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"User LogOut from the Application")]
+        public void WhenUserLogOutFromTheApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Successful LogIN message should display")]
+        public void ThenSuccessfulLogINMessageShouldDisplay()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Successful LogOut message should display")]
+        public void ThenSuccessfulLogOutMessageShouldDisplay()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ UnitTestProject1/ff_001_LogIn_Feature.feature
@@ -0,0 +1,16 @@
+﻿Feature: LogIn_Feature
+ 	In order to access my account
+ 	As a user of TWS IDE
+ 	I want to log into TWS
+ 	 
+ 	@mytag
+ 	Scenario: Successful Login with Valid Credentials
+ 	Given User is at the Login Window
+ 	When User enter UserName and Password
+ 	And Click on the LogIn button
+ 	Then Successful LogIN message should display
+ 	 
+ 	Scenario: Successful LogOut
+ 	Given User is logged in
+ 	When User LogOut from the Application
+ 	Then Successful LogOut message should display
